2ba194e13b1aea4c5a5026383b09f0b3221c2b54
Hrithik Naha <naha.hrithkk@gmail.com>
2019-03-15 19:36:07 +0530

    Initial Commit, with boiler plate for adding in mongoDB

---
+++ mongodb-connect.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
+    if(err){
+        return console.log("Unable to connect to mongoDb servers.");
+    }
+    console.log("Connected to MongoDb servers.");
+    const db = client.db('TodoApp')
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed : false
+    // }, function(err, result){
+    //     if(err){
+    //         return console.log("Unable to insert todo");
+    //     }
+    //     console.log(JSON.stringify(result.ops,undefined, 2))
+    // })
+
+    db.collection('Users').insertOne({
+        name: 'Hrithik Naha',
+        age: 20,
+        location: 'Kolkata, India'
+    }, function(err, result){
+        if(err){
+            return console.log("Unable to insert user");
+        }
+        console.log(JSON.stringify(result.ops,undefined, 2))
+    })
+
+    client.close();
+})
