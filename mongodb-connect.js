d2b071bbf573f38f1cf886020059daa39d1c5810
Hrithik Naha <naha.hrithkk@gmail.com>
2019-03-15 20:04:07 +0530

    Add Fetching data from mongoose

--- mongodb-connect.js
+++ mongodb-connect.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
     if(err){
@@ -7,15 +7,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
     console.log("Connected to MongoDb servers.");
     const db = client.db('TodoApp')
 
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed : false
-    // }, function(err, result){
-    //     if(err){
-    //         return console.log("Unable to insert todo");
-    //     }
-    //     console.log(JSON.stringify(result.ops,undefined, 2))
-    // })
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed : false
+    }, function(err, result){
+        if(err){
+            return console.log("Unable to insert todo");
+        }
+        console.log(JSON.stringify(result.ops,undefined, 2))
+    })
 
     db.collection('Users').insertOne({
         name: 'Hrithik Naha',
---
+++ mongodb-find.js
@@ -0,0 +1,17 @@
+const {MongoClient} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
+    if(err){
+        return console.log("Unable to connect to mongoDb servers.");
+    }
+    console.log("Connected to MongoDb servers.");
+    const db = client.db('TodoApp')
+
+    db.collection('Todos').find().count().then((count), function(){
+        console.log(`Todos Count : ${count}`);
+    }, function(err){
+        console.log("Unable to fetch Todo");
+    })
+
+    // client.close();
+})
