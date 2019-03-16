adbc92d8efceef8068404fff34e680051121ac04
Hrithik Naha <naha.hrithkk@gmail.com>
2019-03-16 09:54:03 +0530

    Add test for  get /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectId} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [
     {
+        _id : new ObjectId(),
         text: 'First Test Todo'
     }, {
+        _id : new ObjectId(),
         text: 'Second test Todo'
     }
 ]
@@ -70,4 +73,32 @@ beforeEach((done) => {
             })
             .end(done)
       })
+  })
+
+  describe('GET /todos/:id', () => {
+    it('should get the todo', (done) => {
+      request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect(function(res){
+          expect(res.body.todo.text).toBe(todos[0].text)
+        })
+        .end(done);
+    })
+
+    it('should return 404 if todo not found', (done) => {
+      var hexId = new ObjectId().toHexString();
+
+      request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done)
+    })
+
+    it('should return 404 for non object ids', (done) => {
+      request(app)
+        .get(`/todos/123`)
+        .expect(404)
+        .end(done)
+    })
   })
