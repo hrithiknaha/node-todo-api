ddf0ee4ec12a06a5bb4fdd2458b2ee1fc3205227
Hrithik Naha <naha.hrithkk@gmail.com>
2019-03-19 20:05:13 +0530

    Indent server-test file

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,9 +1,9 @@
-const expect = require('expect');
-const request = require('supertest');
-const {ObjectId} = require('mongodb');
+const expect      = require('expect');
+const request     = require('supertest');
+const {ObjectId}  = require('mongodb');
 
-const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
+const {app}       = require('./../server');
+const {Todo}      = require('./../models/todo');
 
 const todos = [
     {
