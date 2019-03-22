bd78bc0b8c706f6d7ff919129381d4d86bd5c9f5
Hrithik Naha <naha.hrithkk@gmail.com>
2019-03-22 16:42:09 +0530

    Change test case for User/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -267,7 +267,7 @@ beforeEach(populateTodos);
           }
   
           User.findById(users[1]._id).then((user) => {
-            expect(user.tokens[0]).toInclude({
+            expect(user.tokens[1]).toInclude({
               access: 'auth',
               token: res.headers['x-auth']
             });
@@ -293,7 +293,7 @@ beforeEach(populateTodos);
           }
   
           User.findById(users[1]._id).then((user) => {
-            expect(user.tokens.length).toBe(0);
+            expect(user.tokens.length).toBe(1);
             done();
           }).catch((e) => done(e));
         });
